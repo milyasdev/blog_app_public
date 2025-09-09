df41624d31dafe44102f964cf49cbdcf5c78326d
MUHAMMAD ILYAS <muhammad.ilyas45@gmail.com>
2025-09-09 22:49:35 +0700

    ok

--- resources/views/master/head.blade.php
+++ resources/views/master/head.blade.php
@@ -11,5 +11,4 @@
     <link href="{{ asset('assets/css/plugins.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
 </head>
